fdc60796bd0f2560f8e76048ece43c0d485f2ded
¨TimBunk¨ <timbunk12@kpnmail.nl>
2017-10-16 20:49:21 +0200

    Added visibility unform

--- tb2d/shaders/textHUD.fs
+++ tb2d/shaders/textHUD.fs
@@ -5,7 +5,8 @@ out vec4 theColor;
 in vec2 textureCoord;
 
 uniform sampler2D ourTexture;
+uniform float visibility;
 
 void main() {
-  theColor = texture(ourTexture, textureCoord);
+  theColor = texture(ourTexture, textureCoord) * visibility;
 }
