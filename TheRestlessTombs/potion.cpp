af372658ec39ee65cb79079629962f5fc749a0d8
TimBunk <timbunk12@kpnmail.nl>
2017-11-16 13:42:34 +0100

    Moved TheRestlessTombs to a new repository

--- TheRestlessTombs/potion.cpp
+++
@@ -1,9 +0,0 @@
-#include "potion.h"
-
-Potion::Potion(Camera* camera, Shader* shader, b2World* world) : Item::Item(camera, shader, world) {
-
-}
-
-Potion::~Potion() {
-
-}
--- TheRestlessTombs/shaders/healthbar.fs
+++
@@ -1,9 +0,0 @@
-#version 440 core
-
-out vec4 theColor;
-
-uniform vec4 color;
-
-void main() {
-  theColor = color;
-}
