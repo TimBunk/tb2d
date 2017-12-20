fcbe411a93db516400cbad1061ef69223e33af98
TimBunk <timbunk12@kpnmail.nl>
2017-12-20 11:46:27 +0100

    The rotator now makes use of instancing rendering

--- StaffOfTheChaosTemple/mirror.cpp
+++ StaffOfTheChaosTemple/mirror.cpp
@@ -3,8 +3,10 @@
 Mirror::Mirror(bool rotatable, int width, int height, Texture* texture, Camera * camera, b2World * world) : B2Entity::B2Entity(width, height, glm::vec2(0,0), texture, camera, world)
 {
 	if (rotatable) {
-		rotator = new Rotator(90, 15, glm::vec2(-0.5f, 0.0f), ResourceManager::GetTexture("rotator"), camera, world);
+		rotator = new Rotator(90, 15, glm::vec2(-0.5f, 0.0f), nullptr, camera, world);
 		rotator->localPosition = glm::vec2(-22, 0);
+		rotator->SetInstancedRenderer("rotator");
+		rotator->SetPivot(glm::vec2(-0.5f, 0.0f));
 		rotator->CreateBoxCollider(90, 15, glm::vec2(-0.5f, 0.0f), false, false);
 		this->AddChild(rotator);
 	}
@@ -28,7 +30,9 @@ Mirror::~Mirror()
 
 void Mirror::Update(double deltaTime)
 {
-	localAngle = rotator->GetRotation();
+	if (rotator != nullptr) {
+		localAngle = rotator->GetRotation();
+	}
 }
 
 void Mirror::SetRotation(float degrees)
