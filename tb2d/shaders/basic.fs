23448ebca7d7ed375885d09bf1de188e41b46f26
TimBunk <timbunk12@kpnmail.nl>
2017-12-26 17:57:39 +0100

    Added a basic shader

---
+++ tb2d/shaders/basic.fs
@@ -0,0 +1,24 @@
+#version 440 core
+
+in VS_OUT
+{
+  vec2 textureCoord;
+  float textureID;
+  vec4 color;
+} fs_in;
+
+out vec4 color;
+
+uniform sampler2D textures[32];
+
+void main() {
+  if (fs_in.textureID > 0.0) {
+    int tID = int(fs_in.textureID);
+    tID -= 1;
+    color = texture(textures[tID], fs_in.textureCoord);
+    color += fs_in.color;
+  }
+  else {
+    color = fs_in.color;
+  }
+}
