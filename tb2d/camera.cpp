06ffc946e1700f20295780df1b78b312f8055336
TimBunk <timbunk12@kpnmail.nl>
2018-01-18 09:49:11 +0100

    Added documentation

--- tb2d/camera.cpp
+++ tb2d/camera.cpp
@@ -2,6 +2,8 @@
 
 Camera::Camera(int width, int height)
 {
+
+	// Set the projection to point at the center of the screen.
 	projection = glm::ortho(((float)width) / 2 * -1, ((float)width)/2, ((float)height)/2 * -1, ((float)height)/2, -50.0f, 50.0f);
 	position = glm::vec2(0.0f, 0.0f);
 	angle = 0.0f;
@@ -16,12 +18,14 @@ Camera::~Camera()
 
 void Camera::SetPosition(glm::vec2 position)
 {
+	// Position times -1 to give the user the illusion that camera is moving to that position
 	position *= -1;
 	this->position = position;
 }
 
 void Camera::PositionAdd(glm::vec2 position)
 {
+	// Position times -1 to give the user the illusion that camera is moving to that position
 	position *= -1;
 	this->position += position;
 }
@@ -33,46 +37,11 @@ glm::vec2 Camera::GetPosition()
 	return positionCopy;
 }
 
-void Camera::SetAngle(float angle)
-{
-	this->angle = angle;
-}
-
-void Camera::AngleAdd(float angle)
-{
-	this->angle += angle;
-}
-
-float Camera::GetAngle()
-{
-	return angle;
-}
-
-void Camera::SetScale(float scale)
-{
-	this->scale = scale;
-}
-
 glm::mat4 Camera::GetViewMatrix()
 {
 	glm::mat4 view;
 	view = glm::translate(view, glm::vec3(position.x, position.y, 0.0f));
-	view = glm::rotate(view, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+	view = glm::rotate(view, angle, glm::vec3(0.0f, 0.0f, 1.0f));
 	view = glm::scale(view, glm::vec3(scale, scale, 0.0f));
 	return view;
-}
-
-glm::mat4 Camera::GetProjectionMatrix()
-{
-	return projection;
-}
-
-float Camera::GetWidth()
-{
-	return width;
-}
-
-float Camera::GetHeight()
-{
-	return height;
 }
--- tb2d/camera.h
+++ tb2d/camera.h
@@ -1,47 +1,80 @@
+/**
+* @file camera.h
+*
+* @brief The Camera header file.
+*/
+
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include <iostream>
-
-#include <GL/glew.h>
 #include <glm-0.9.8.4/glm/glm.hpp>
 #include <glm-0.9.8.4/glm/gtc/matrix_transform.hpp>
 #include <glm-0.9.8.4/glm/gtc/type_ptr.hpp>
 
-#include <GL/glew.h>
-
+/**
+* @brief The Camera class
+*/
 class Camera {
 public:
+	/// @brief Constructor of the Camera
+	/// @param screenWidth is the width that will be used by the projectionMatrix
+	/// @param screenHeight is the height that will be used by the projectionMatrix
 	Camera(int screenWidth, int screenHeight);
+	/// @brief Destructor of the Camera
 	virtual ~Camera();
 
-	// Position in pixels
+	/// @brief SetPosition sets the position of the camera
+	/// @param position is the new position of the camera
+	/// @return void
 	void SetPosition(glm::vec2 position);
+	/// @brief PositionAdd adds a value to the currentposition
+	/// @param position is the value that will be added to thee currentPosition
+	/// @return void
 	void PositionAdd(glm::vec2 position);
+	/// @brieff GetPosition return the currentposition of the camera
+	/// @return glm::vec2
 	glm::vec2 GetPosition();
 
-	// Angle in degrees
-	void SetAngle(float angle);
-	void AngleAdd(float angle);
-	float GetAngle();
+	/// @brief SetAngle sets the angle in radians
+	/// @param angle is the new angle
+	/// @return void
+	void SetAngle(float angle) { this->angle = angle; }
+	/// @brief AngleAdd adds a value to the currentangle in radians
+	/// @param angle is the angle that will be added to the currentangle
+	/// @return void
+	void AngleAdd(float angle) { this->angle += angle; }
+	/// @brief return the currentangle in radians
+	/// @return float
+	float GetAngle() { return angle; }
 
-	void SetScale(float scale);
+	/// @brief SetScale sets the scale of the Camera
+	/// @param scale is the new scale of the Camera
+	/// @return void
+	void SetScale(float scale) { this->scale = scale; }
 
+	/// @brief GetViewMatrix return the viewmatrix of the Camera
+	/// @return glm::mat4
 	glm::mat4 GetViewMatrix();
-	glm::mat4 GetProjectionMatrix();
+	/// @brief GetProjectionMatrix return the projectionmatrix of the Camera
+	/// @return glm::mat4
+	glm::mat4 GetProjectionMatrix() { return projection; }
 
-	float GetWidth();
-	float GetHeight();
+	/// @brief GetWidth return width of the Camera. Also note that the Camera pivot is in the center of the screen
+	/// @return float
+	float GetWidth() { return width; }
+	/// @brief GetHeight return the height of the Camera. Also note that the Camera pivot is in the center of the screen
+	/// @return float
+	float GetHeight() { return height; }
 
 private:
-	float width;
-	float height;
+	float width; ///< @brief width is the width that will be used by the projectionmatrix
+	float height; ///< @brief height is the height that will be used by the projectionmatrix
 
-	glm::vec2 position;
-	float angle;
-	float scale;
+	glm::vec2 position; ///< @brief position is the position of the Camera
+	float angle; ///< @brief angle is the angle in radians of the Camera
+	float scale; ///< @brief scale is the scale of the camera
 
-	glm::mat4 projection;
+	glm::mat4 projection; ///< @brief projection is the projectionmatrix of the camera
 };
 
 #endif // !CAMERA_H
